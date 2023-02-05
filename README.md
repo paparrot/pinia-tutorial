9d5c2b81b8f0b23b2e4d3f0810f49e470c0ccb80
Klim <subbotin.klim@gmail.com>
2023-02-05 13:00:42 +0100

    add movie store to app, get data from movie store in app

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Simple movies app build with vue 3 and pinia as state manager.
 
 # Roadmap
-- [ ] State
+- [x] State
 - [ ] Getters
 - [ ] Actions
 - [ ] Async actions
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,63 @@
+<template>
+  <main>
+    <header class="header">
+      <img src="/logo.svg" alt="Logo" class="header-logo">
+      <h2>My favourite movies</h2>
+    </header>
+    <div class="tabs">
+      <button :class="['btn', {'btn_green': movieStore.activeTab === 1}]">Search</button>
+      <button :class="['btn', {'btn_green': movieStore.activeTab === 2}]">Favourite</button>
+    </div>
+    <div class="movies" v-if="movieStore.activeTab === 1">
+      <h3>All Movies</h3>
+      <Movie
+          v-for="movie of movieStore.movies"
+          :key="movie.id"
+          :movie="movie" />
+    </div>
+    <div class="search" v-else>
+      Search
+    </div>
+  </main>
+</template>
+
 <script setup>
+import {useMovieStore} from "./stores/MovieStore.js";
+import Movie from "./components/Movie.vue";
+
+const movieStore = useMovieStore();
 </script>
 
-<template>
-</template>
+<style lang="css">
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+.header-logo {
+  max-width: 50px;
+  margin-right: 10px;
+}
+.btn {
+  border: none;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  margin: 0 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  background: #efefef;
+}
+.btn:hover {
+  opacity: 0.7;
+}
+.btn_green {
+  background: #37df5c;
+}
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+</style>
