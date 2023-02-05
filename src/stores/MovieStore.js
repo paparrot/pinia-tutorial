9f6ff7914200395541b28269cb9462a76b58a273
Klim <subbotin.klim@gmail.com>
2023-02-05 14:29:32 +0100

    replace logic to composition api

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,28 +1,67 @@
 import {defineStore} from 'pinia';
+import {computed, ref} from "vue";
 
-export const useMovieStore = defineStore('movieStore',{
-    state: () => ({
-        movies: [],
-        activeTab: 1,
-    }),
-    getters: {
-        watchedMovies() {
-            return this.movies.filter(el => el.isWatched)
-        },
-        totalCountMovies() {
-            return this.movies.length;
-        }
-    },
-    actions: {
-        setActiveTab(id) {
-            this.activeTab = id;
-        },
-        toggleWatched(id) {
-            const idx = this.movies.findIndex(el => el.id === id);
-            this.movies[idx].isWatched = !this.movies[idx].isWatched;
-        },
-        deleteMovie(id) {
-            this.movies = this.movies.filter(item => item.id !== id);
-        }
+
+// Option API
+// export const useMovieStore = defineStore('movieStore',{
+//     state: () => ({
+//         movies: [],
+//         activeTab: 1,
+//     }),
+//     getters: {
+//         watchedMovies() {
+//             return this.movies.filter(el => el.isWatched)
+//         },
+//         totalCountMovies() {
+//             return this.movies.length;
+//         }
+//     },
+//     actions: {
+//         setActiveTab(id) {
+//             this.activeTab = id;
+//         },
+//         toggleWatched(id) {
+//             const idx = this.movies.findIndex(el => el.id === id);
+//             this.movies[idx].isWatched = !this.movies[idx].isWatched;
+//         },
+//         deleteMovie(id) {
+//             this.movies = this.movies.filter(item => item.id !== id);
+//         }
+//     }
+// });
+
+// Composition API
+export const useMovieStore = defineStore('movieStore', () => {
+    const movies = ref([])
+    const activeTab = ref(1);
+
+    const watchedMovies = computed(() => {
+       return movies.value.filter(item => item.isWatched)
+    })
+    const totalCountMovies = computed(() => {
+        return movies.value.length
+    })
+
+    const setActiveTab = (id) => {
+        activeTab.value = id;
     }
-});
+
+    const toggleWatched = (id) => {
+        const idx = movies.value.findIndex(el => el.id === id);
+        movies.value[idx].isWatched = !movies.value[idx].isWatched;
+    }
+
+    const deleteMovie = (id) => {
+        movies.value = movies.value.filter(item => item.id !== id);
+    }
+
+    return {
+        movies,
+        activeTab,
+        watchedMovies,
+        totalCountMovies,
+        setActiveTab,
+        toggleWatched,
+        deleteMovie
+    }
+})
--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -1,29 +1,61 @@
 import {defineStore} from "pinia";
+import {ref} from 'vue';
 import {useMovieStore} from "./MovieStore.js";
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_ENDPOINT = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
-export const useSearchStore = defineStore('searchStore', {
-    state: () => ({
-        movies: [],
-        loader: false,
-    }),
-    actions: {
-        async getMovies(search) {
-            this.loader = true;
-            const res = await fetch(`${API_ENDPOINT}${search}`)
-            const data = await res.json();
-            this.movies = data.results;
-            this.loader = false;
-        },
-        addToUserMovies(movie) {
-            const movieStore = useMovieStore();
-            movieStore.movies.push({
-                ...movie,
-                isWatched: false,
-            })
-            this.movies = this.movies.filter(item => item.id !== movie.id)
-        }
+// Option API
+// export const useSearchStore = defineStore('searchStore', {
+//     state: () => ({
+//         movies: [],
+//         loader: false,
+//     }),
+//     actions: {
+//         async getMovies(search) {
+//             this.loader = true;
+//             const res = await fetch(`${API_ENDPOINT}${search}`)
+//             const data = await res.json();
+//             this.movies = data.results;
+//             this.loader = false;
+//         },
+//         addToUserMovies(movie) {
+//             const movieStore = useMovieStore();
+//             movieStore.movies.push({
+//                 ...movie,
+//                 isWatched: false,
+//             })
+//             this.movies = this.movies.filter(item => item.id !== movie.id)
+//         }
+//     }
+// })
+
+// Composition API
+export const useSearchStore = defineStore('searchStore', () => {
+    const loader = ref(false);
+    const movies = ref([]);
+    const movieStore = useMovieStore();
+
+    const getMovies = async (search) => {
+        loader.value = true;
+        const res = await fetch(`${API_ENDPOINT}${search}`)
+        const data = await res.json();
+        movies.value = data.results;
+        loader.value = false;
+    }
+
+    const addToUserMovies = (movie) => {
+        movieStore.movies.push({
+            ...movie,
+            isWatched: false,
+        })
+        movies.value = movies.value.filter(item => item.id !== movie.id)
+    }
+
+    return {
+        loader,
+        movies,
+        getMovies,
+        addToUserMovies
     }
 })
